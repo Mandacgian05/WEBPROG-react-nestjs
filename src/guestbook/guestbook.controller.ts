e8a1812ed71ee122a6cfc48164a0a2dc21a40435
Gian Mandac <gsmandac@student.apc.edu.ph>
2026-02-13 07:03:16 +0000

    fix: allow POST requests for guestbook

--- src/guestbook/guestbook.controller.ts
+++ src/guestbook/guestbook.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common'; // Ensure Post and Body are here
 import { GuestbookService } from './guestbook.service';
 
-@Controller('guestbook') // <--- If this is empty, the URL won't work
+@Controller('guestbook')
 export class GuestbookController {
   constructor(private readonly guestbookService: GuestbookService) {}
 
   @Get()
   listPosts() {
     return this.guestbookService.findAll();
+  }
+
+  @Post() // This is the part that handles your curl command
+  addPost(@Body() body: { name: string; message: string }) {
+    return this.guestbookService.create(body);
   }
 }
