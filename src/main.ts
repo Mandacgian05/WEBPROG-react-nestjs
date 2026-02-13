3e1c60c24cbd91aa53a56dea4d2300e49b9f2ce9
Gian Mandac <gsmandac@student.apc.edu.ph>
2026-02-13 09:25:23 +0000

    none

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors(); // This allows your frontend to talk to the API
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  // Serves the public folder for the frontend UI
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
